a26b6b7788c26e8e927a78f1826471f8e2756550
Elaine <elainezuniga@gmail.com>
2014-10-08 21:25:39 -0500

    I did homework things

--- js-animal-gallery-master/gallery/script.js
+++ js-animal-gallery-master/gallery/script.js
@@ -1,1 +1,14 @@
 // Add your code here
+
+
+// $('.filter-button').on('click', function () {
+//   alert("Yup, it's a button.");
+// });
+
+$('.filter-button').on('click', function (){
+	$('.' + this.id).toggle();
+	$('#' + this.id).toggleClass('active');
+	alertify.log(this.id.toUpperCase());
+})
+
+
